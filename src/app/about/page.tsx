fb3cb090bc8fd15e168328cfd2e851f3b53df074
AleksIvanov195 <alexsandar196@gmail.com>
2025-03-24 20:24:16 +0000

    remove default files

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+export default function AboutPage() {
+  console.log("Hello from there!");
+  
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>This page is server-rendered</p>
+    </div>
+  );
+}
