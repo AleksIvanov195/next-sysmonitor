bfaf8768e313359631c75d2fd4a90be8ec5029eb
AleksIvanov195 <alexsandar196@gmail.com>
2025-03-25 18:56:38 +0000

    feat: initial structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
 export default function Home() {
   return (
     <main className="min-h-screen">
-      {/* Your custom content goes here */}
-      <h1 className="text-4xl font-bold">Your App Name</h1>
+			      <h1 className="text-4xl font-bold">My app</h1>
     </main>
   );
 }
