c2dd2603026195a56460e45ae5d3cde401a6c6d1
AleksIvanov195 <alexsandar196@gmail.com>
2025-03-29 20:36:33 +0000

    feat: -Improved Home component with styling and backgrounds - can fetch system info data - the systeminfo logic is moved to a file in /lib for better structure and re-usability

--- src/app/api/system-info/route.ts
+++ src/app/api/system-info/route.ts
@@ -1,42 +1,8 @@
 import { NextResponse } from 'next/server';
-import si from 'systeminformation';
+import { getSystemInfo } from '@/lib/systemInfo';
 
-interface CpuInfo {
-  manufacturer: string;
-  brand: string;
-  speed: number;
-  cores: number;
-}
-
-interface MemoryInfo {
-  total: number;
-  used: number;
-  free: number;
-}
-
-interface DiskInfo {
-  fs: string;
-  size: number;
-  used: number;
-  mount: string;
-}
-
-interface SystemLoad {
-  avgLoad: number;
-  currentLoad: number;
-}
 
 export async function GET() {
-	const [cpu, memory, disk, currentLoad] : [CpuInfo, MemoryInfo, DiskInfo[], SystemLoad] = await Promise.all([
-		si.cpu(),
-		si.mem(),
-		si.fsSize(),
-		si.currentLoad(),
-	]);
-	return NextResponse.json({
-		cpu,
-		memory,
-		disk,
-		currentLoad,
-	});
+  const systemInfo = await getSystemInfo();
+  return NextResponse.json(systemInfo);
 }
---
+++ src/lib/systemInfo.ts
@@ -0,0 +1,38 @@
+// src/lib/systemInfo.ts
+import si from "systeminformation";
+
+export interface CpuInfo {
+  manufacturer: string;
+  brand: string;
+  speed: number;
+  cores: number;
+}
+
+export interface MemoryInfo {
+  total: number;
+  used: number;
+  free: number;
+}
+
+export interface DiskInfo {
+  fs: string;
+  size: number;
+  used: number;
+  mount: string;
+}
+
+export interface SystemLoad {
+  avgLoad: number;
+  currentLoad: number;
+}
+
+export async function getSystemInfo() {
+  const [cpu, memory, disk, currentLoad]: [
+    CpuInfo,
+    MemoryInfo,
+    DiskInfo[],
+    SystemLoad
+  ] = await Promise.all([si.cpu(), si.mem(), si.fsSize(), si.currentLoad()]);
+
+  return { cpu, memory, disk, currentLoad };
+}
